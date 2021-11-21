bbcc487bf6e411044cf85fe86aa69ecbb83e8ac1
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-11-21 10:03:16 +0100

    [atmanager] refactor done, updated Dockerfile, docs

--- sh_tool/bin/center.sh
+++ sh_tool/bin/center.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # @brief   Apache Tomcat Server Manager
-# @version ver.2.0
+# @version ver.3.0
 # @date    Sat Nov 20 11:40:40 CET 2021
 # @company None, free software to use 2021
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
